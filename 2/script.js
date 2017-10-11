6287119f1685a8f81dcf4c063a0171ffe82fd1d1
Eidvilas <32447357+Eidvilas@users.noreply.github.com>
2017-10-11 23:48:09 +0300

    Elektro markto forma

---
+++ 2/script.js
@@ -0,0 +1,18 @@
+
+
+
+
+    
+        function rodyti_fizini() {
+            document.getElementById('div_fizinis').style.display = "block";
+            document.getElementById('div_imone').style.display = "none";
+            document.getElementById('btn_1').style.backgroundColor = "#C70726";
+            document.getElementById('btn_2').style.backgroundColor = "white";
+        }
+
+        function rodyti_imone() {
+            document.getElementById('div_imone').style.display = "block";
+            document.getElementById('div_fizinis').style.display = "none";
+            document.getElementById('btn_2').style.backgroundColor = "#C70726"
+            document.getElementById('btn_1').style.backgroundColor = "white";
+        }
